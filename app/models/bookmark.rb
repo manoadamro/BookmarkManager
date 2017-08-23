8765169ec5f49b0c3af21ddbb69b3f24632604ed
Adam Romano <manoadamro.imp@gmail.com>
2017-08-23 16:21:34 +0100

    and again...

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,9 +3,17 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 DataMapper.setup(
-  :default, ENV['DATABASE_URL'] ||
-  "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"
+  :default,
+  "postgres://isynnryfyejdfx:" \
+  "9872efef4477145fb14a1a5e2fec790887dfdf48b7d56dd0e4beeebae9c07333" \
+  "@ec2-54-225-119-223.compute-1.amazonaws.com" \
+  ":5432/ddoo4ijuh4ul5m/bookmark_manager_#{ENV['RACK_ENV']}"
 )
+
+# DataMapper.setup(
+#   :default, ENV['DATABASE_URL'] ||
+#   "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}"
+# )
 
 # lib/bookmarks.rb
 class Bookmark
